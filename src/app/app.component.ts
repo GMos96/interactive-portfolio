a25532311cc8a879e2e5126843baca192df9168b
Garrett Moseley <moseleyg96@gmail.com>
2022-04-14 13:37:35 -0400

    Part of homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'gm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'interactive-portfolio';
+
 }
